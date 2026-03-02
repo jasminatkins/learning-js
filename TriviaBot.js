fa7982c03d10ccbd315d7f6e5cca2f6dca4ecc82
Jasmin Atkins <Jasmin.Atkins@bloomsbury.com>
2026-03-02 20:34:10 +0000

    add trivia bot

---
+++ TriviaBot.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Fun Fact Guide";
+let botLocation = "Planet Earth";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Did you know that, as of 2025, 98.9% of websites use " + favoriteLanguage + "?";
+
+console.log(codingFact);
+
+codingFact = "Did you know that the first version of " + favoriteLanguage + " was developed over a period of just 10 days!?";
+
+console.log(codingFact);
+
+codingFact = "Did you know that " + favoriteLanguage + " was born in 1995?";
+
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
